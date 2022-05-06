29fa2cb48a1c6ac882f0c92940c720625af64dae
brenekv.03 <brenekv.03@A321-15>
2022-05-06 10:50:21 +0200

    Prvni knihovvna

---
+++ CelaCisla/Operace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CelaCisla
+{
+    public class Operace
+    {
+        public static int Soucet(int x,int y)
+        {
+            return x + y;
+        }
+        public static int CelociselnyPodil(int x, int y,out int zbytek)
+        {
+            zbytek = x % y;
+            int podil = x / y;
+            return podil;
+        }
+    }
+}
---
+++ Vypocty/Form1.cs
@@ -0,0 +1,36 @@
+﻿using CelaCisla;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vypocty
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            MessageBox.Show("Součet je: " +  Operace.Soucet(x, y));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            int zbytek = 0;
+            MessageBox.Show("Podíl je: " + Operace.CelociselnyPodil(x, y, out zbytek) + " a zbytek je: " + zbytek); 
+        }
+    }
+}
